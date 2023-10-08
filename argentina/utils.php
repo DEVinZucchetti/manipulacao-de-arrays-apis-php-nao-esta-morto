116a51828f8d853978e955d461572612c1e90714
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-07 21:49:00 -0300

    feat: cria o arquivo utils.php e suas funções ao projeto

---
+++ argentina/utils.php
@@ -0,0 +1,13 @@
+<?php 
+function getBody() {
+    return json_decode(file_get_contents("php://input")); // pegar o body no formato de string 
+}
+
+function readFileContent($fileName){
+    return json_decode(file_get_contents($fileName));
+}
+
+function saveFileContents($fileName, $content) {
+    file_put_contents($fileName, json_encode($content));
+}
+?>
