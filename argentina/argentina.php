d8737e90e2d1756fb5a7e4cc9689da6ab5d3b2c5
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-07 22:35:48 -0300

    feat: cria o metodo post e validações

--- argentina/argentina.php
+++ argentina/argentina.php
@@ -1,7 +1,21 @@
-<?php 
+<?php
 require_once 'config.php';
 require_once 'utils.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-?>
+if ($method === 'POST') {
+    $body = getBody(); //pega o body ao concluir o post 
+
+    $name = validateString($body->name);
+    $contact = validateString($body->contact);
+    $opening_hours = validateString($body->opening_hours);
+    $description = validateString($body->description);
+    $latitude = filter_var($body->latitude, FILTER_VALIDATE_FLOAT);
+    $longitude = filter_var($body->longitude, FILTER_VALIDATE_FLOAT);
+
+    if(!$name || !$contact || !$opening_hours || !$description || !$latitude || !$longitude) {
+        responseError(400, 'Faltaram informações!');
+    }
+}
+?> 
