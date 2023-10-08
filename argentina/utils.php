7da554d7da17ef59e44cf8a8a86c6dba45722deb
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-07 22:29:41 -0300

    feat: cria resposta de erro

--- argentina/utils.php
+++ argentina/utils.php
@@ -1,17 +1,26 @@
-<?php 
-function getBody() {
+<?php
+function getBody()
+{
     return json_decode(file_get_contents("php://input")); // pegar o body no formato de string 
 }
 
-function readFileContent($fileName){
+function readFileContent($fileName)
+{
     return json_decode(file_get_contents($fileName));
 }
 
-function saveFileContents($fileName, $content) {
+function saveFileContents($fileName, $content)
+{
     file_put_contents($fileName, json_encode($content));
 }
 
-function validateString($value) {
+function validateString($value)
+{
     return filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS); // filtra o nome dentro o nome dentro do body e pega os dados 
 }
-?>
+
+function responseError($message, $status) // respota de erro ao fazer o post do body
+{
+    http_response_code($status);
+    echo json_encode(['error' => $message]);
+}
