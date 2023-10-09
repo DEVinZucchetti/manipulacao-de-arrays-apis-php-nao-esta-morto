a831006065465d16fd8d717ec5e9bfc2c0d36387
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-08 21:41:10 -0300

    feat: cria um filtro para verificar a repetição de um item cadastrado

--- argentina/argentina.php
+++ argentina/argentina.php
@@ -7,6 +7,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($method === 'POST') {
     $body = getBody(); //pega o body ao concluir o post 
 
+    //validação dos dados informados no body
     $name = validateString($body->name);
     $contact = validateString($body->contact);
     $opening_hours = validateString($body->opening_hours);
@@ -14,12 +15,19 @@ if ($method === 'POST') {
     $latitude = filter_var($body->latitude, FILTER_VALIDATE_FLOAT);
     $longitude = filter_var($body->longitude, FILTER_VALIDATE_FLOAT);
 
+    //error caso algum dado não seja informado 
     if (!$name || !$contact || !$opening_hours || !$description || !$latitude || !$longitude) {
         responseError(400, 'Faltaram informações!');
     }
 
     //fazer a leitura do arquivo primeiro para depois salvar
     $allData = readFileContent(FILE_CITY);
+
+    //faz o filtro para verificar se não tem um name repetido
+    $itemWhitSameName = array_filter($allData, function ($item) use($name) {
+        return $item->name === $name;
+    });
+
 
     // salvando as informações dentro dos arquivos com o array associativo  
     $data = [
