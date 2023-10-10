1fd4294f76e41b3384edaab8cda993c15cc197db
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-10-10 16:02:11 -0500

    "refactor: cadastro lugares-post"
    Criando funcoes

--- equador/equador.php
+++ equador/equador.php
@@ -1,30 +1,40 @@
 <?php
-header("Content-Type: application/json"); // a aplicação retorna json
-header("Access-Control-Allow-Origin: *"); // vai aceitar requisições de todas origens
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // habilita métodos
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+require_once "config.php";
+require_once "utils.php";
 
 $method = $_SERVER["REQUEST_METHOD"];
+
+// pego body
 if($method === "POST"){
-    $body= json_decode(file_get_contents("php://input"));
+    $body= getBody();
 
-    var_dump($body);
+// valido os dados 
+    $name = sanitizeString($body ->name);
+    $contact = sanitizeString($body ->contact);
+    $opening_hours = sanitizeString($body ->opening_hours);
+    $description= sanitizeString($body ->description);
+    $latitude = filter_var($body ->latitude, FILTER_VALIDATE_FLOAT);
+    $longitude = filter_var($body ->longitude, FILTER_VALIDATE_FLOAT);
+   
 
-    $name = filter_var($body ->name, FILTER_SANITIZE_SPECIAL_CHARS);
-    $contact = filter_var($body ->contact, FILTER_SANITIZE_SPECIAL_CHARS);
-    $opening = filter_var($body ->opening, FILTER_SANITIZE_SPECIAL_CHARS);
-    $description = filter_var($body ->description, FILTER_SANITIZE_SPECIAL_CHARS);
-    $latitude = filter_var($body ->latitude, FILTER_SANITIZE_SPECIAL_CHARS);
-    $longitude = filter_var($body ->longitude, FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if(!$name || !$contact || !$opening ||!$description || !$latitude ||!$longitude){
-        echo json_encode(["error" => "Preencha todas as informacoes"]);
+    if(!$name || !$contact || !$opening_hours ||!$description || !$latitude ||!$longitude){
+        responseError("Faltaram informacoes esenciais",400);
+       
     }
 
-    $equador = json_decode (file_get_contents("equador.txt")) ;
-    array_push($equador, ["name"=>$name, "contact" => $contact, "opening" => $opening, "description" => $description, "latitude " => $latitude , "longitude " => $longitude ]);
-    file_put_contents("equador.txt",json_encode($equador));
-    var_dump( $equador);
+    $data = [
+        "name" => $name,
+        "contact" => $contact,
+        "opening_hours" => $opening_hours,
+        "description" => $description,
+        "latitude" => $latitude,
+        "longitude" => $longitude,
+    ];
 
-    
-}
+    $allData = readFileContent(FILE_CITY);
+    array_push($allData, $data);
+    saveFileContent(FILE_CITY, $allData);
+
+    response($data, 201);
+ }
+?>
