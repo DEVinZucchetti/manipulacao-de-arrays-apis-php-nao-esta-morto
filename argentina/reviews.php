9ffdbd5b341cbe30a37899e067a713cefac6f0c1
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-15 19:29:43 -0300

    feat: pega os dados do body e faz a validação

--- argentina/reviews.php
+++ argentina/reviews.php
@@ -2,6 +2,17 @@
 require_once 'config.php';
 require_once 'utils.php';
 
+$method = $_SERVER['REQUEST_METHOD'];
 
+if ($method === 'POST') {
+    $body = getBody();
+
+    $place_id = sanitizeInput($body, 'place_id', FILTER_VALIDATE_INT);
+    $name = sanitizeInput($body, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = sanitizeInput($body, 'email', FILTER_VALIDATE_EMAIL);
+    $stars = sanitizeInput($body, 'stars', FILTER_VALIDATE_FLOAT);
+    $date = (new DateTime())->format('d/m/Y h:m');
+    $status = sanitizeInput($body, 'status', FILTER_SANITIZE_SPECIAL_CHARS);
+}
 
 ?>
