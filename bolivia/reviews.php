a73431ee3ee11cc6060c73848261580b332b81f9
Caroline Sampaio <caroline_08022@hotmail.com>
2023-10-15 21:50:13 -0300

    feat: adiciona cadastro de avaliação para lugar

---
+++ bolivia/reviews.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'config.php';
+require_once 'utils.php';
+require_once 'models/Review.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$blacklist = ['polimorfismo',  'herança', 'abstração', 'encapsulamento'];
+
+if ($method === 'POST') {
+    $body = getBody();
+
+    $place_id = sanitizeInput($body, 'place_id', FILTER_VALIDATE_INT);
+    $name = sanitizeInput($body, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = sanitizeInput($body, 'email', FILTER_VALIDATE_EMAIL);
+    $stars = sanitizeInput($body, 'stars', FILTER_VALIDATE_FLOAT);
+
+    if (!$place_id) responseError('ID do local ausente. Insira para prosseguir.', 400);
+    if (!$name) responseError('Descrição da avaliação ausente. Insira para prosseguir.', 400);
+    if (!$email) responseError('Email informado inválido. Insira corretamente para prosseguir.', 400);
+    if (!$stars) responseError('Quantidade de estrelas ausente. Insira para prosseguir.', 400);
+
+    if (strlen($name) > 200) responseError('O texto ultrapassou o limite de 200 caracteres.', 400);
+
+    foreach ($blacklist as $word) {
+        if (str_contains(strtolower($name), $word)) {
+            $name = str_ireplace($word, '[EDITADO PELO ADMIN]', $name);
+        }
+    }
+
+    $review = new Review($place_id);
+    $review->setName($name);
+    $review->setEmail($email);
+    $review->setStars($stars);
+    $review->save();
+
+    response(['message' => 'Avaliação enviada com sucesso. Após a análise, ela ficará visível para todos.'], 201);
+}
