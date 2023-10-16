a57d263af337966fb889df223633ecfeb68f6e8b
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-16 13:08:46 -0300

    feat: define o get do place id

--- argentina/reviews.php
+++ argentina/reviews.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'config.php';
 require_once 'utils.php';
 require_once 'models/Review.php';
@@ -15,31 +15,33 @@ if ($method === 'POST') {
 
     $prohibited_words = ['polimorfismo', 'herança', 'abstração', 'encapsulamento'];
 
-    if(!$place_id) responseError(400,'Id do lugar ausente');
-    if(!$name) responseError(400,'Descrição da avaliação ausente');
-    if(!$email) responseError(400,'Email ausente');
-    if(!$stars) responseError(400,'Quantidade de estrelas ausente');
-    if(!$status) responseError(400,'Status de avaliação ausente');
+    if (!$place_id) responseError(400, 'Id do lugar ausente');
+    if (!$name) responseError(400, 'Descrição da avaliação ausente');
+    if (!$email) responseError(400, 'Email ausente');
+    if (!$stars) responseError(400, 'Quantidade de estrelas ausente');
+    if (!$status) responseError(400, 'Status de avaliação ausente');
 
-    if(strlen($name) > 200) responseError(400, 'O texto ultrapassou o limite');
+    if (strlen($name) > 200) responseError(400, 'O texto ultrapassou o limite');
 
-    foreach($prohibited_words as $word) {
-        if(str_contains(strtolower($name), $word)) {
-           $name = str_ireplace($word, '[EDITADO PELO ADMIN]', $name);
+    foreach ($prohibited_words as $word) {
+        if (str_contains(strtolower($name), $word)) {
+            $name = str_ireplace($word, '[EDITADO PELO ADMIN]', $name);
         }
     }
 
 
     // maneira de salvar um arquivo 
     $review = new Review($place_id);
-    $review->setName($name); 
-    $review->setEmail($email); 
-    $review->setStars($stars); 
-    $review->setStatus($status);  
-    $review-> save();
+    $review->setName($name);
+    $review->setEmail($email);
+    $review->setStars($stars);
+    $review->setStatus($status);
+    $review->save();
 
     response(201, ['message' => 'Cadastrado com sucesso!']);
+} else if ($method === 'GET') {
 
-} else if($method === 'GET') {
-    
+    $place_id = filter_var($_GET, 'id', FILTER_VALIDATE_INT);
+
+    if (!$place_id) responseError(400, 'ID do lugar esta ausente'); 
 }
