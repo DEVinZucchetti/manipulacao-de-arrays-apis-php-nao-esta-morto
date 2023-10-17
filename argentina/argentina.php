44f7b0168a5073a833d99a5a4fc92ec8d5e340fb
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-17 18:19:26 -0300

    feat: ajuste na sintaxe e importação do controller para o arquivo argentina.php

--- argentina/argentina.php
+++ argentina/argentina.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'config.php';
-require_once 'utils.php';
-require_once 'models/Place.php';
+require_once 'controller/PlaceController.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -12,37 +11,9 @@ if ($method === 'POST') {
 } else if ($method === 'GET' && !isset($_GET['id'])) {
     $controller->list();
 } else if ($method === 'DELETE') {
-    $id = filter_var($_GET['id'], FILTER_SANITIZE_SPECIAL_CHARS);
-    if (!$id) {
-        responseError(400, 'ID ausente');
-    }
-
-    $place = new Place();
-    $place->delete($id);
-
-
-    response(204, ['message' => 'Deletado com sucesso!']);
+    $controller->delete();
 } else if ($method === 'GET' && $_GET['id']) {
-    $id = filter_var($_GET['id'], FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if (!$id) {
-        responseError(400, 'ID ausente');
-    }
-
-    $place = new Place();
-    $item = $place->listOne($id);
-    response(200, $item);
+    $controller->listOne();
 } else if ($method === 'PUT') {
-
-    $body = getBody();
-    $id = filter_var($_GET['id'], FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if (!$id) {
-        responseError(400, 'ID ausente');
-    }
-
-    $place = new Place();
-    $place->update($id, $body);
-
-    response(200, ['message' => 'Atualizado com sucesso!']);
+    $controller->update();
 }
--- argentina/controller/PlaceController.php
+++ argentina/controller/PlaceController.php
@@ -1,46 +1,94 @@
-<?php 
+<?php
 
-class PlaceController {
-    public function create() {
+require_once 'utils.php';
+require_once 'models/Place.php';
+class PlaceController
+{
+    public function create()
+    {
         $body = getBody();
 
-    $name = validateString($body->name);
-    $contact = validateString($body->contact);
-    $opening_hours = validateString($body->opening_hours);
-    $description = validateString($body->description);
-    $latitude = filter_var($body->latitude, FILTER_VALIDATE_FLOAT);
-    $longitude = filter_var($body->longitude, FILTER_VALIDATE_FLOAT);
+        $name = validateString($body->name);
+        $contact = validateString($body->contact);
+        $opening_hours = validateString($body->opening_hours);
+        $description = validateString($body->description);
+        $latitude = filter_var($body->latitude, FILTER_VALIDATE_FLOAT);
+        $longitude = filter_var($body->longitude, FILTER_VALIDATE_FLOAT);
 
 
-    if (!$name || !$contact || !$opening_hours || !$description || !$latitude || !$longitude) {
-        responseError(400, 'Faltaram informações!');
+        if (!$name || !$contact || !$opening_hours || !$description || !$latitude || !$longitude) {
+            responseError(400, 'Faltaram informações!');
+        }
+
+
+        $allData = readFileContent(FILE_CITY);
+
+
+        $itemWithSameName = array_filter($allData, function ($item) use ($name) {
+            return $item->name === $name;
+        });
+
+        if (count($itemWithSameName) > 0) {
+            responseError(409, 'O item já existe');
+        }
+
+        $place = new Place($name);
+        $place->setContact($contact);
+        $place->setOpening_hours($opening_hours);
+        $place->setDescription($description);
+        $place->setLatitude($latitude);
+        $place->setLongitude($longitude);
+        $place->save();
+
+        response(201, ['message' => 'Cadastrado com sucesso!']);
     }
 
-
-    $allData = readFileContent(FILE_CITY);
-
-
-    $itemWithSameName = array_filter($allData, function ($item) use ($name) {
-        return $item->name === $name;
-    });
-
-    if (count($itemWithSameName) > 0) {
-        responseError(409, 'O item já existe');
-    }
-
-    $place = new Place($name);
-    $place->setContact($contact);
-    $place->setOpening_hours($opening_hours);
-    $place->setDescription($description);
-    $place->setLatitude($latitude);
-    $place->setLongitude($longitude);
-    $place->save();
-
-    response(201, ['message' => 'Cadastrado com sucesso!']);
-    }
-
-    public function list() {
+    public function list()
+    {
         $places = (new Place())->list();
         response(200, $places);
+    }
+
+    public function delete()
+    {
+        $id = filter_var($_GET['id'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (!$id) {
+            responseError(400, 'ID ausente');
+        }
+
+        $place = new Place();
+        $place->delete($id);
+
+        response(200, ['message' => 'Deletado com sucesso!']);
+    }
+
+    public function listOne()
+    {
+        $id = filter_var($_GET['id'], FILTER_SANITIZE_SPECIAL_CHARS);
+        if (!$id) {
+            responseError(400, 'ID ausente');
+        }
+
+        $place = new Place();
+        $place->delete($id);
+
+
+        response(204, ['message' => 'Deletado com sucesso!']);
+    }
+
+    public function update()
+    {
+        $body = getBody();
+        $id = filter_var($_GET['id'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (!$id) {
+            responseError(400, 'ID ausente');
+        }
+
+        $place = new Place();
+        $place->update($id, $body);
+
+        response(200, ['message' => 'Atualizado com sucesso!']);
     }
 }
