fca4829b21b6ced7c495fa6a9df7f1d68329dcb5
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-10-17 13:13:30 -0300

    feat: cria função de create no arquivo placecontroller

--- argentina/controller/PlaceController.php
+++ argentina/controller/PlaceController.php
@@ -2,7 +2,55 @@
 
 class PlaceController {
     public function create() {
-        
+        $body = getBody();
+
+    $name = validateString($body->name);
+    $contact = validateString($body->contact);
+    $opening_hours = validateString($body->opening_hours);
+    $description = validateString($body->description);
+    $latitude = filter_var($body->latitude, FILTER_VALIDATE_FLOAT);
+    $longitude = filter_var($body->longitude, FILTER_VALIDATE_FLOAT);
+
+
+    if (!$name || !$contact || !$opening_hours || !$description || !$latitude || !$longitude) {
+        responseError(400, 'Faltaram informações!');
+    }
+
+
+    $allData = readFileContent(FILE_CITY);
+
+
+    $itemWithSameName = array_filter($allData, function ($item) use ($name) {
+        return $item->name === $name;
+    });
+
+    if (count($itemWithSameName) > 0) {
+        responseError(409, 'O item já existe');
+    }
+
+    $place = new Place($name);
+    $place->setContact($contact);
+    $place->setOpening_hours($opening_hours);
+    $place->setDescription($description);
+    $place->setLatitude($latitude);
+    $place->setLongitude($longitude);
+    $place->save();
+
+    $data = [
+        'id' => $_SERVER['REQUEST_TIME'],
+        'name' => $name,
+        'contact' => $contact,
+        'opening_hours' => $opening_hours,
+        'description' => $description,
+        'latitude' => $latitude,
+        'longitude' => $longitude
+    ];
+    
+    array_push($allData, $data);
+
+    saveFileContent(FILE_CITY, $data);
+
+    response(201, ['message' => 'Cadastrado com sucesso!']);
     }
 }
 
