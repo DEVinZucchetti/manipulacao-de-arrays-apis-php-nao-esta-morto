c9b1c9c55beb7de2d2f4ad763af7737b0bac4d9e
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-10-21 00:16:56 -0500

    feat: ex.4 cria classe e variáveis

---
+++ equador/models/Place.php
@@ -0,0 +1,13 @@
+<?php 
+class Place{
+    public $id;
+    private $name;
+    private $contact;
+    private $opening_hours;
+    private $description;
+    private $latitude;
+    private $longitude;
+
+   
+}
+?>
