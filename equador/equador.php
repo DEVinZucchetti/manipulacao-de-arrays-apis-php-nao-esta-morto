fb6d5e385257e87575b7021e59c497fb8381da58
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-10-21 21:31:11 -0500

    feat: ex.6 importo place controller

--- equador/equador.php
+++ equador/equador.php
@@ -1,7 +1,6 @@
 <?php
 require_once "config.php";
-require_once "utils.php";
-require_once "models/Place.php";
+require_once "controller/PlaceController.php";
 
 $method = $_SERVER["REQUEST_METHOD"];
 
