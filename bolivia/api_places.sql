bba8e7c890784c2bf638973d7adc16c2fc7303e3
Caroline Sampaio <caroline_08022@hotmail.com>
2023-10-26 10:17:08 -0300

    feat: adiciona ex 3 - tabela reviews

--- bolivia/api_places.sql
+++ bolivia/api_places.sql
@@ -12,3 +12,18 @@ CREATE TABLE places (
   longitude float UNIQUE NOT NULL,
   created_at timestamp WITH time zone DEFAULT NOW()
 );
+
+-- exercício 3 - Tabela Reviews
+CREATE TYPE status_reviews AS enum('PENDENTE', 'APROVADO', 'REJEITADO');
+
+CREATE TABLE reviews(
+id serial PRIMARY KEY,
+place_id integer,
+ name text NOT NULL,
+ email varchar(150),
+ stars decimal(2,1),
+ date timestamp,
+ status status_reviews DEFAULT 'PENDENTE',
+ created_at timestamp WITH time zone DEFAULT NOW(),
+ FOREIGN KEY (place_id) REFERENCES places(id)
+)
