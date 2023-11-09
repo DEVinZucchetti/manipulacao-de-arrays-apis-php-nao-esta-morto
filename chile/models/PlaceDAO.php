d489bc1783d958813db98b75983680a54f0e3905
HenriqueRodriguesRossi <hrodriguescruciakrossi46@gmail.com>
2023-11-09 15:58:33 -0300

    Exercício 04 finalizado

--- chile/models/PlaceDAO.php
+++ chile/models/PlaceDAO.php
@@ -1,4 +1,13 @@
-public function getConnection() {
+<?php
+
+class PlaceDAO {
+    private $connection;
+
+    public function __construct(PDO $connection) {
+        $this->connection = $connection;
+    }
+
+    public function getConnection() {
         return $this->connection;
     }
 
@@ -42,7 +51,7 @@ public function getConnection() {
     }
 
     public function findMany() {
-        $sql = "SELECT * FROM places order by name";
+        $sql = "SELECT * FROM places ORDER BY name";
 
         $statement = ($this->getConnection())->prepare($sql);
         $statement->execute();
@@ -50,4 +59,49 @@ public function getConnection() {
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
+
+    public function update(Place $place) {
+        try {
+            $sql = "UPDATE places SET
+                name = :name,
+                contact = :contact,
+                opening_hours = :opening_hours,
+                description = :description,
+                latitude = :latitude,
+                longitude = :longitude
+            WHERE id = :id";
+
+            $statement = ($this->getConnection())->prepare($sql);
+
+            $statement->bindValue(':id', $place->getId());
+            $statement->bindValue(':name', $place->getName());
+            $statement->bindValue(':contact', $place->getContact());
+            $statement->bindValue(':opening_hours', $place->getOpeningHours());
+            $statement->bindValue(':description', $place->getDescription());
+            $statement->bindValue(':latitude', $place->getLatitude());
+            $statement->bindValue(':longitude', $place->getLongitude());
+
+            $statement->execute();
+
+            return ['success' => true];
+        } catch (PDOException $error) {
+            debug($error->getMessage());
+            return ['success' => false];
+        }
+    }
+
+    public function delete($id) {
+        try {
+            $sql = "DELETE FROM places WHERE id = :id";
+            $statement = ($this->getConnection())->prepare($sql);
+            $statement->bindValue(':id', $id);
+            $statement->execute();
+
+            return ['success' => true];
+        } catch (PDOException $error) {
+            debug($error->getMessage());
+            return ['success' => false];
+        }
+    }
 }
+?>
