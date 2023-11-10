97e93af6210dfb6927deddb1d36e7e4c4d8cf1e0
Drew <84882983+vdr3w@users.noreply.github.com>
2023-11-10 13:12:02 -0300

    Ex5

---
+++ brasil/models/ReviewDAO.php
@@ -0,0 +1,61 @@
+<?php
+require_once 'Database.php';
+
+class ReviewDAO extends Database
+
+{
+ public function insert(Review $review)
+ {
+
+  try {
+
+   $sql = "insert into places
+                    (
+                        place_id,
+                        name,
+                        email,
+                        stars,
+                        date,
+                        status
+                    )
+                    values 
+                    (
+                        :place_id_value,
+                        :name_value,
+                        :email_value,
+                        :stars_value,
+                        :date_value,
+                        :status_value
+                    )
+        ";
+
+   $statement = ($this->getConnection())->prepare($sql);
+
+   $statement->bindValue(":name_value", $review->getName());
+   $statement->bindValue(":email_value", $review->getEmail());
+   $statement->bindValue(":stars_value", $review->getStars());
+   $statement->bindValue(":date_value", $review->getDate());
+   $statement->bindValue(":status_value", $review->getStatus());
+
+
+   $statement->execute();
+
+   return ['sucess' => true];
+  } catch (PDOException $error) {
+   debug($error->getMessage());
+   return ['sucess' => false];
+  }
+ }
+
+ public function list($place_id)
+ {
+  $sql = "SELECT * from reviews where place_id = :id_value";
+
+  $statement = ($this->getConnection())->prepare($sql);
+  $statement->bindValue(":id_value", $place_id);
+  $statement->execute();
+
+  $result = $statement->fetchALL(PDO::FETCH_ASSOC);
+  return $result;
+ }
+}
