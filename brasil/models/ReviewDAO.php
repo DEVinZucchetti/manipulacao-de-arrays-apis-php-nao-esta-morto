1fff5edba381cbe5548dc9231cfb64098716a5e8
Drew <84882983+vdr3w@users.noreply.github.com>
2023-11-10 13:12:27 -0300

    Ex6

--- brasil/models/ReviewDAO.php
+++ brasil/models/ReviewDAO.php
@@ -47,6 +47,16 @@ class ReviewDAO extends Database
   }
  }
 
+ public function update($id, $status)
+ {
+  $sql = "UPDATE reviews SET status = :status WHERE id = :id_value";
+
+  $statement = ($this->getConnection())->prepare($sql);
+  $statement->bindValue(":status", $status);
+  $statement->bindValue(":id_value", $id);
+  $statement->execute();
+ }
+
  public function list($place_id)
  {
   $sql = "SELECT * from reviews where place_id = :id_value";
