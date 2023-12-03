5014c37c4c1ca80280ebdfe704e9904b24576844
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2023-12-03 19:30:49 -0300

    exercicio 2 Paraguai

---
+++ paraguai/config.php
@@ -0,0 +1,6 @@
+<?php
+
+header("Content-Type: application/json"); // a aplicação retorna json
+header("Access-Control-Allow-Origin: *"); // vai aceitar requisições de todas origens
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // habilita métodos
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
