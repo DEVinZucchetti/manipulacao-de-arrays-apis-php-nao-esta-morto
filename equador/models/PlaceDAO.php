6f41e4c0efca21be91396a4ee58d1ea1d82b0b6b
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-29 02:20:27 -0500

    feat: crio os metodos que vai ter o banco de dados

--- equador/models/PlaceDAO.php
+++ equador/models/PlaceDAO.php
@@ -8,6 +8,26 @@ class PlaceDAO{
         $this->connection = new PDO("pgsql:host=localhost;dbname=api_places","docker","docker");
     }
 
+    public function insert(){
+
+    }
+
+    public function findMany(){
+
+    }
+
+    public function findOne(){
+
+    }
+
+    public function deleteOne(){
+
+    }
+
+    public function updateOne(){
+
+    }
+
   
     public function getConnection()
     {
