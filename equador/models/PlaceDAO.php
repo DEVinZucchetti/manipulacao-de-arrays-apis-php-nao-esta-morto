41b7b9249ea3a2fbee3589886821e79dd8ba1511
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-29 02:14:18 -0500

    feat:Crio arquivo plaeDAO.php
    colocas as credencias do banco do dados

---
+++ equador/models/PlaceDAO.php
@@ -0,0 +1,11 @@
+<?php 
+
+class PlaceDAO{
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = new PDO("pgsql:host=localhost;dbname=api_places","docker","docker");
+    }
+
+}
